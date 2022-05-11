04cb214eb41ed7ab404c0f3f5f37099664bc41a6
Jean <jean.soro007@gmail.com>
2022-05-11 16:34:21 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import { createClient } from 'contentful'
+import { RecipeCards } from '../components/RecipeCards';
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({
+    content_type: 'recipe'
+  })
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
+
+
+export default function Recipes({ recipes }) {
+  console.log(recipes)
+  return (
+    <div className="recipe-list">
+      {
+        recipes.map((recipe) => {
+          return (
+            <RecipeCards key={recipe.sys.id} recipe={recipe} />
+          )
+        })
+      }
+
+
+      <style jsx>
+        {
+          ` 
+            .recipe-list{
+              display: grid;
+              grid-template-columns :1fr 1fr;
+              grid-gap: 20px 40px;
+            }
+          `
+        }
+      </style>
+
+    </div>
+  )
+}
